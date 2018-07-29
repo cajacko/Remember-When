465d2fb6432fdd69a813d6ce33c771a8f45e3708
Charlie Jackson <charlie@charliejackson.com>
2018-07-29 09:14:23 +0100

    Added entry component

---
+++ src/components/App/App.render.js
@@ -0,0 +1,12 @@
+// @flow
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => (
+  <View>
+    <Text>Hello</Text>
+  </View>
+);
+
+export default App;
---
+++ src/entry.js
@@ -0,0 +1,5 @@
+// @flow
+
+import App from './components/App';
+
+export const ENTRY_COMPONENT = App;
