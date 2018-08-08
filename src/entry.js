c536302f2caeb498fd715cacb65c051f8bc5f829
Charlie Jackson <charlie@charliejackson.com>
2018-08-08 19:25:35 +0100

    Ensure the safe area view colours

--- src/entry.js
+++ src/entry.js
@@ -1,5 +1,14 @@
 // @flow
 
+import safeAreaViewSubscriptions from '@cajacko/lib/dist/utils/safeAreaViewSubscriptions';
+import { BLACK } from '@cajacko/lib/dist/config/styles/colors';
 import { ENTRY } from './config/routes';
+
+safeAreaViewSubscriptions.set(null, null, {
+  topColor: BLACK,
+  bottomColor: BLACK,
+  statusBarColor: BLACK,
+  statusBarStyle: 'light-content',
+});
 
 export const ROUTES = ENTRY;
