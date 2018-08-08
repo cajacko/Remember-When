ef710d29a43929e66585ea5d04dac62234d85580
Charlie Jackson <charlie@charliejackson.com>
2018-08-08 08:44:56 +0100

    Split out tab nav items

--- src/components/Layout/ContentWithTabNav/ContentWithTabNav.render.js
+++ src/components/Layout/ContentWithTabNav/ContentWithTabNav.render.js
@@ -3,27 +3,15 @@
 import React from 'react';
 import CJContentWithTabNav from '@cajacko/lib/dist/components/Layout/ContentWithTabNav';
 import { withRouter } from '@cajacko/lib/dist/lib/react-router';
-import { HOME, PLUS } from '@cajacko/lib/dist/config/icons';
+import mapTabNavItemsWithPush from '@cajacko/lib/dist/utils/mapTabNavItemsWithPush';
 import isTabNavItemActive from '../../../utils/conditionals/isTabNavItemActive';
+import tabNavItems from '../../../config/tabNavItems';
 
 const ContentWithTabNav = ({ children, location, history: { push } }) => (
   <CJContentWithTabNav
     tabNav={{
       isActive: isTabNavItemActive(location),
-      items: [
-        {
-          key: '1',
-          text: 'Home',
-          icon: HOME,
-          action: () => push('/'),
-        },
-        {
-          key: '2',
-          text: 'Add',
-          icon: PLUS,
-          action: () => push('/newPost'),
-        },
-      ],
+      items: mapTabNavItemsWithPush(tabNavItems, push),
     }}
   >
     {children}
---
+++ src/config/tabNavItems.js
@@ -0,0 +1,18 @@
+// @flow
+
+import { HOME, PLUS } from '@cajacko/lib/dist/config/icons';
+
+export default [
+  {
+    key: '1',
+    text: 'Home',
+    icon: HOME,
+    route: '/',
+  },
+  {
+    key: '2',
+    text: 'Add',
+    icon: PLUS,
+    route: '/newPost',
+  },
+];
