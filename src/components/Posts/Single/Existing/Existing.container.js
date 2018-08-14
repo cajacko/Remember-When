696dabb96f257ee8e36fba0e43d1973e6c8378e4
Charlie Jackson <charlie@charliejackson.com>
2018-08-14 21:35:13 +0100

    Error boundary if single post has not data

--- src/components/Posts/Single/Existing/Existing.container.js
+++ src/components/Posts/Single/Existing/Existing.container.js
@@ -2,6 +2,8 @@
 
 import { withRouter } from '@cajacko/lib/dist/lib/react-router';
 import { connect } from '@cajacko/lib/dist/lib/react-redux';
+import withErrorBoundaryIfDataNotFound from '@cajacko/lib/dist/components/HOCs/withErrorBoundaryIfDataNotFound';
+import errors from '@cajacko/lib/dist/config/errors';
 import PostsSingleCommon from '../Common';
 import mapSinglePost from '../../../../utils/containers/mapSinglePost';
 
@@ -14,6 +16,12 @@ export const mapStateToProps = (
   }
 ) => mapSinglePost(state, id);
 
-const PostsSingleCommonWithRedux = connect(mapStateToProps)(PostsSingleCommon);
+const PostsSingleCommonWithDataCheck = withErrorBoundaryIfDataNotFound(
+  PostsSingleCommon,
+  errors[4],
+  'Posts/Single/Existing/Existing.container did not receive any data'
+);
+
+const PostsSingleCommonWithRedux = connect(mapStateToProps)(PostsSingleCommonWithDataCheck);
 
 export default withRouter(PostsSingleCommonWithRedux);
