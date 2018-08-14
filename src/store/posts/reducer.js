64b37fd7ca87e228d84a6b2bee6bf298eaf2ac0c
Charlie Jackson <charlie@charliejackson.com>
2018-08-14 06:35:32 +0100

    Sort posts

--- src/store/posts/reducer.js
+++ src/store/posts/reducer.js
@@ -7,6 +7,12 @@ import { SAVE_POST_ACTION, DELETE_POST_ACTION } from './actions';
 
 const initialState = fromJS(defaultState);
 
+const getPostDateByID = (state, id) => {
+  const timestamp = state.getIn(['postsByID', id, 'date']);
+
+  return new Date(timestamp);
+};
+
 export default createReducer(initialState, {
   [SAVE_POST_ACTION]: (state, {
     id, content, date, isNewPost,
@@ -17,7 +23,17 @@ export default createReducer(initialState, {
       newState = newState.set('list', newState.get('list').unshift(id));
     }
 
-    return newState.setIn(['postsByID', id], Map({ id, content, date }));
+    newState = newState.setIn(['postsByID', id], Map({ id, content, date }));
+
+    newState = newState.set(
+      'list',
+      newState
+        .get('list')
+        .sort((a, b) =>
+          (getPostDateByID(newState, a) < getPostDateByID(newState, b) ? 1 : -1))
+    );
+
+    return newState;
   },
   [DELETE_POST_ACTION]: (state, { id }) =>
     state.set('list', state.get('list').filter(postId => postId !== id)),
