bd1afb5d7e34dafaa4a2a38a516d7203564070cf
Charlie Jackson <charlie@charliejackson.com>
2018-08-15 08:13:03 +0100

    Make use of section util from lib

--- src/utils/selectors/orderedPostsWithSectionsSelector.js
+++ src/utils/selectors/orderedPostsWithSectionsSelector.js
@@ -1,54 +1,12 @@
 // @flow
 
-import { List, Map } from 'immutable';
 import { createSelector } from 'reselect';
+import sectionListByMonth from '@cajacko/lib/dist/utils/sectionListByMonth';
 import orderedPostsSelector from './orderedPostsSelector';
-
-const isNewMonth = (dateA, dateB) => {
-  if (!dateA) return true;
-
-  if (
-    dateA.getMonth() === dateB.getMonth() &&
-    dateA.getFullYear() === dateB.getFullYear()
-  ) {
-    return false;
-  }
-
-  return true;
-};
 
 const orderedPostsWithSectionsSelector = createSelector(
   orderedPostsSelector,
-  ({ posts }) => {
-    let sections = List();
-    let section;
-    let prevDate;
-
-    posts.forEach((post) => {
-      const date = new Date(post.get('date'));
-
-      if (isNewMonth(prevDate, date)) {
-        if (section) {
-          sections = sections.push(section);
-        }
-
-        section = Map({
-          date,
-          data: List(),
-        });
-      }
-
-      section = section.set('data', section.get('data').push(post));
-
-      prevDate = date;
-    });
-
-    if (section) {
-      sections = sections.push(section);
-    }
-
-    return { posts: sections };
-  }
+  ({ posts }) => ({ posts: sectionListByMonth(posts) })
 );
 
 export default orderedPostsWithSectionsSelector;
