b77654bf4fd5703f7eae010d190d7ff0fcdd6aae
Charlie Jackson <charlie@charliejackson.com>
2018-11-08 11:21:27 +0000

    🐛 fix(docker): Pass in env vars from host to docker
    - Original Branch: master

--- run.sh
+++ run.sh
@@ -1,13 +1,29 @@
-./build.sh
+# ./build.sh
 
 docker stop rw
 docker rm rw
 
 set -e
 
-# docker run -d -it --name=rw remember-when tail -f /dev/null
-docker run -d -it --name=rw remember-when yarn deploy -t main-app --deploy-env alpha-deploygate --android
-# docker exec -it rw /bin/bash
+command="docker run"
+
+# Get all the env keys and pass in from the host
+str=$(egrep -v '^#' .env | xargs)
+IFS=" "
+ary=($str)
+for key in "${!ary[@]}";
+do
+  str2="${ary[$key]}"
+  IFS="="
+  ary2=($str2)
+  command="${command} -e ${ary2[0]}"
+done
+
+command="${command} -d -it --name=rw remember-when yarn deploy -t main-app --deploy-env alpha-deploygate --android"
+
+# Execute the docker run command with all the env set
+eval $command
+
 docker logs --follow rw
 docker stop rw
 docker rm rw
